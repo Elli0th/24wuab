f562e78fd22b4ffe36a31ba0d374840c3e272fba
Elli0th <101704853+Elli0th@users.noreply.github.com>
2024-03-12 10:29:21 +0100

    12-3-24
    Lagt till ett bildspel och bilder och förbättrar sidans läsbarhet och prestanda

--- js/main.js
+++ js/main.js
@@ -23,3 +23,22 @@ const navSlide = () => {
 };
 
 navSlide();
+
+// Your existing JavaScript code
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("my-slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
