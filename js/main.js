5dbd8a2841b155407d4da81e0f166fc5a292b343
Elli0th <101704853+Elli0th@users.noreply.github.com>
2024-03-19 10:33:47 +0100

    19/3-2024

--- js/main.js
+++ js/main.js
@@ -42,3 +42,46 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+const logoLink = document.querySelector(".logo-link");
+const normalLogo = document.querySelector(".logo-image.normal");
+const invertedLogo = document.querySelector(".logo-image.inverted");
+
+logoLink.addEventListener("mouseenter", () => {
+  normalLogo.classList.add("hidden");
+  invertedLogo.classList.remove("hidden");
+});
+
+logoLink.addEventListener("mouseleave", () => {
+  normalLogo.classList.remove("hidden");
+  invertedLogo.classList.add("hidden");
+});
+
+const changeMobileImages = () => {
+  const slides = document.querySelectorAll(".my-slides img");
+  const windowWidth = window.innerWidth;
+
+  slides.forEach((slide) => {
+    if (windowWidth <= 780) {
+      // Change image sources for mobile size
+      if (slide.alt === "Burger Meal") {
+        slide.src = "img/burger_meal.webp";
+      } else if (slide.alt === "A person holding a burger") {
+        slide.src = "img/holding_burger.webp";
+      }
+    } else {
+      // Change back to original image sources for larger screens
+      if (slide.alt === "Burger Meal") {
+        slide.src = "img/burger_meal_16_by_9.webp";
+      } else if (slide.alt === "A person holding a burger") {
+        slide.src = "img/holding_burger_16_by_9.webp";
+      }
+    }
+  });
+};
+
+// Call the function initially to set images according to screen size
+changeMobileImages();
+
+// Listen for window resize events to adjust images if necessary
+window.addEventListener("resize", changeMobileImages);
